4963020d61fa25a39355a8f9e29df6fceb51902a
Robert Baldwin <robbaldwin95@gmail.com>
2014-06-26 12:47:35 -0500

    Add pch file to branch ubuntu-fixes

---
+++ server/src/pch.h
@@ -0,0 +1,34 @@
+#include "../../shared/src/utility.h"
+#include "../../shared/src/Transaction.h"
+#include "../../shared/src/common.h"
+#include "../../shared/src/logging.h"
+#include "../../shared/src/packets.h"
+
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/unordered_map.hpp>
+#include <boost/asio.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/level.hpp>
+#include <boost/serialization/tracking.hpp>
+#include <boost/program_options.hpp>
+#include <boost/serialization/unordered_map.hpp>
+
+#include <vector>
+#include <string>
+#include <exception>
+#include <iostream>
+#include <unordered_map>
+#include <thread>
+#include <functional>
+#include <chrono>
+#include <sstream>
+#include <arpa/inet.h>
+#include <iosfwd>
+#include <cassert>
+#include <future>
+#include <fstream>
+#include <memory>
+#include <mutex>
+#include <deque>
+#include <condition_variable>
